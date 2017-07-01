434d5a44acc3d87b5eb4adca7094c99ca9db5adb
nidhik <nidhikulkarni82@gmail.com>
2017-06-30 21:14:26 -0700

    Reduce time delays for ask show and table delete

--- poker/src/server/GameController.cpp
+++ poker/src/server/GameController.cpp
@@ -1119,7 +1119,7 @@ void GameController::stateAskShow(Table *t)
 	{
 #ifndef SERVER_TESTING
 		// handle player timeout
-		const int timeout = 4;   // FIXME: configurable
+		const int timeout = 1;   // FIXME: configurable
 		if ((int)difftime(time(NULL), t->timeout_start) > timeout || p->sitout)
 		{
 			// default on showdown is "to show"
@@ -1569,7 +1569,7 @@ int GameController::tick()
 	else if (ended)
 	{
 		// delay before game gets deleted
-		if ((unsigned int) difftime(time(NULL), ended_time) >= 60) // used to be 4 min
+		if ((unsigned int) difftime(time(NULL), ended_time) >= 5) // used to be 4 min
 		{
 			// remove all players
 			for (players_type::iterator e = players.begin(); e != players.end();)
